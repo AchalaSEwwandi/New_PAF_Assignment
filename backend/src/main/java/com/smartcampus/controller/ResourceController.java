9b7a7c5836448578d41669aa1f5f5f65462fc457
AchalaSewwandi <158697050+AchalaSEwwandi@users.noreply.github.com>
2026-04-23 15:32:31 +0530

    resource controller update

--- backend/src/main/java/com/smartcampus/controller/ResourceController.java
+++ backend/src/main/java/com/smartcampus/controller/ResourceController.java
@@ -1,18 +1,29 @@
 package com.smartcampus.controller;
 
-import com.smartcampus.dto.ResourceRequest;
-import com.smartcampus.exception.ResourceNotFoundException;
-import com.smartcampus.model.Resource;
-import com.smartcampus.service.ResourceService;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.smartcampus.dto.ResourceRequest;
+import com.smartcampus.exception.ResourceNotFoundException;
+import com.smartcampus.model.Resource;
+import com.smartcampus.service.ResourceService;
 
+import jakarta.validation.Valid;
+//api/resources endpoints 
 @RestController
 @RequestMapping("/api/resources")
 @CrossOrigin(origins = "*")
